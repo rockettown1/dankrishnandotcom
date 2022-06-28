99a32c4e0b140c876ff49bd8317beb61eafd4cb1
Dan Krishnan <dan@wearecodenation.com>
2022-06-28 11:43:48 +0100

    chore: contentful connection

--- cms/contentfulClient.js
+++ cms/contentfulClient.js
@@ -1,5 +1,6 @@
 import { createClient } from "contentful";
 
+//Contentful Access
 export const client = createClient({
   space: process.env.CONTENTFUL_SPACE,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
