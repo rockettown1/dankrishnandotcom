38a0e1b561ad4f54c9a482f83c01565320ba6aa1
Dan Krishnan <dan@wearecodenation.com>
2022-06-28 11:32:30 +0100

    test(unit): added withRouter utility

--- utils/testUtils.tsx
+++ utils/testUtils.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { ThemeProvider } from "styled-components";
+import NextRouter from "next/router";
 import { darkTheme, lightTheme } from "../styles/themes";
 
 export const withTheme = (Component: React.FunctionComponent, theme?: string) => {
@@ -7,4 +9,24 @@ export const withTheme = (Component: React.FunctionComponent, theme?: string) =>
       <Component />
     </ThemeProvider>
   );
+};
+
+export const withRouter = (Component: React.FunctionComponent): React.FunctionComponent => {
+  const useRouter = jest.spyOn(NextRouter, "useRouter" as any);
+
+  useRouter.mockImplementation(() => ({
+    route: "/",
+    pathname: "",
+    query: "",
+    asPath: "",
+    push: jest.fn(),
+    events: {
+      on: jest.fn(),
+      off: jest.fn(),
+    },
+    beforePopState: jest.fn(() => null),
+    prefetch: jest.fn(() => null),
+  }));
+
+  return () => <Component />;
 };
