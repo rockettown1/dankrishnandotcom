9a55f77ec23e78984ecc96ba69435c9ac2e595e4
Dan Krishnan <dan@wearecodenation.com>
2022-07-14 10:58:20 +0100

    fix(prisma): add prisma setup for Nextjs dev environment

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,3 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+/* 
+This is to handle Nextjs hot reloading in development which instantiates new prisma instances 
+Source: https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
+*/
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined;
+}
+
+export const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ["query"],
+  });
+
+if (process.env.NODE_ENV !== "production") global.prisma = prisma;
