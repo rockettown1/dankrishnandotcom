06377456b1aa7edf2fa9bf0f390e556fe73fee11
Dan Krishnan <dan@wearecodenation.com>
2022-07-18 01:31:02 +0100

    chore: debugging prism CI error

--- prisma/index.ts
+++ prisma/index.ts
@@ -5,16 +5,10 @@ This is to handle Nextjs hot reloading in development which instantiates new pri
 Source: https://www.prisma.io/docs/guides/database/troubleshooting-orm/help-articles/nextjs-prisma-client-dev-practices
 */
 
-declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// declare global {
+//   // allow global `var` declarations
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
 
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
-
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
+export const prisma = new PrismaClient();
